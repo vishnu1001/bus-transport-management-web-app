19eafd67a3b42e5839feb421835548a9df0da549
vishnu <vishnuramguru@gmail.com>
2022-06-20 11:56:29 +0530

    no-edit

--- php/login.php
+++ php/login.php
@@ -1,31 +1,33 @@
+
 <?php
 
-$servername="localhost";
-$username="root";
-$password="";
-$db="amrita_bus";
+    $servername="localhost";
+    $username="root";
+    $password="";
+    $db="amrita_bus";
 
-$conn=mysqli_connect($servername,$username,$password,$db);
+    $conn=mysqli_connect($servername,$username,$password,$db);
 
-if(!$conn)
-{
-    echo "Connection failed " .mysqli_connect_error($conn);
-    die();
-}
+    if(!$conn)
+    {
+        echo "Connection failed " .mysqli_connect_error($conn);
+        die();
+    }
 
-$mail=$_POST['mail'];
-$pass=$_POST['pass'];
+    $mail=$_POST['mail'];
+    $pass=$_POST['pass'];
 
-$sql="INSERT INTO registered (email,pass_word) VALUES ('$mail','$pass')";
+    $sql="INSERT INTO registered (email,pass_word) VALUES ('$mail','$pass')";
 
-if(mysqli_query($conn,$sql))
-{
-        echo " You've successfully registered to Amrita bus facilities"; 
-}
-else
-{
-    echo " Failed to register! " .mysqli_error($conn);
-}
+    if(mysqli_query($conn,$sql))
+    {
+            echo " You've successfully registered to Amrita bus facilities"; 
+    }
+    else
+    {
+        echo " Failed to register! " .mysqli_error($conn);
+    }
 
-mysqli_close($conn);
+    mysqli_close($conn);
+    
 ?>
