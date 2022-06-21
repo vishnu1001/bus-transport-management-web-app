56748bd1e76f9058b9b9b20abb661e05d2886e01
saketh1001 <psaketh2002@gmail.com>
2022-06-21 11:42:13 +0530

    php and html[name] changes

--- php/signup.php
+++ php/signup.php
@@ -20,8 +20,13 @@
     $reg=$_POST['reg'];
     $course=$_POST['course'];
     $sem=$_POST['sem'];
+    $door_no=$_POST['door_no'];
+    $street_name=$_POST['street_name'];
+    $landmark=$_POST['landmark'];
+    $pincode=$_POST['pincode'];
 
-    $sql="INSERT INTO registered (email,student_name,pass_word,reg_no,course,sem) VALUES ('$mail','$name','$pass','$reg','$course','$sem')";
+    $sql="INSERT INTO registered (email,student_name,pass_word,reg_no,course,sem,door_no,street_name,landmark,pincode) VALUES
+          ('$mail','$name','$pass','$reg','$course','$sem','$door_no','$street_name','$landmark','$pincode')";
 
     if(mysqli_query($conn,$sql))
     {
