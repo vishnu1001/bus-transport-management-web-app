209de25ce3871048c3a2d3fe6745be499dee374a
vishnu <vishnuramguru@gmail.com>
2022-07-10 15:58:22 +0530

    no change

--- php/login.php
+++ php/login.php
@@ -1,31 +1,32 @@
 <?php
-$servername="localhost";
-$username="root";
-$password="";
-$database="amrita_bus";
+
+    $servername="localhost";
+    $username="root";
+    $password="";
+    $database="amrita_bus";
 
 
-$mail=$_POST['mail'];
-$pass=$_POST['pass'];
+    $mail=$_POST['mail'];
+    $pass=$_POST['pass'];
 
-$conn=mysqli_connect($servername,$username,$password,$database);
+    $conn=mysqli_connect($servername,$username,$password,$database);
 
-if(!$conn)
-{
-    echo "Error creating" .mysqli_connect_error(); die();
-}
+    if(!$conn)
+    {
+        echo "Error creating" .mysqli_connect_error(); die();
+    }
 
-$sql="select * from registered where email='$mail'and pass_word='$pass'";
-$res=mysqli_query($conn,$sql);
-$count=mysqli_num_rows($res);
-if($count)
-{
-    echo "<h1> LOGGED IN! </h1>";
-}
-else
-{
-    echo " <h4> No user present </h4>";
-   // echo "<a href= \"create.html\"> No user? Create an account! </a>";
-}  
+    $sql="select * from registered where email='$mail'and pass_word='$pass'";
+    $res=mysqli_query($conn,$sql);
+    $count=mysqli_num_rows($res);
+    if($count)
+    {
+        echo "<h1> LOGGED IN! </h1>";
+    }
+    else
+    {
+        echo " <h4> No user present </h4>";
+    // echo "<a href= \"create.html\"> No user? Create an account! </a>";
+    }  
 
 ?>
