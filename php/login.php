98f9104dfcf0562c819de6698ce11c0b562f16a8
vishnu <vishnuramguru@gmail.com>
2022-07-10 17:27:02 +0530

    code edited

--- php/login.php
+++ php/login.php
@@ -1,31 +1,69 @@
 <?php
-$servername="localhost";
-$username="root";
-$password="";
-$database="amrita_bus";
+
+    session_start();
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "amrita_bus";
 
 
-$mail=$_POST['mail'];
-$pass=$_POST['pass'];
+    $mail = $_POST['mail'];
+    $pass = $_POST['pass'];
 
-$conn=mysqli_connect($servername,$username,$password,$database);
+    $conn = mysqli_connect($servername,$username,$password,$database);
 
-if(!$conn)
-{
-    echo "Error creating" .mysqli_connect_error(); die();
-}
+    if(!$conn)
+    {
+        echo "Error creating" .mysqli_connect_error(); die();
+    }
 
-$sql="select * from registered where email='$mail'and pass_word='$pass'";
-$res=mysqli_query($conn,$sql);
-$count=mysqli_num_rows($res);
-if($count)
-{
-    echo "<h1> LOGGED IN! </h1>";
-}
-else
-{
-    echo " <h4> No user present </h4>";
- 
-}  
+    $sql = "select * from registered where email='$mail'and pass_word='$pass'";
+    $res = mysqli_query($conn,$sql);
+    $count = mysqli_num_rows($res);
+
+    if($count)
+    {
+        ///echo "<h1> LOGGED IN! </h1>";
+        
+        if(isset($_SESSION['mail']))
+        {
+            print("Loggin in...");
+            print("
+            
+            <html>
+                <head>
+                    <meta http-equiv=\"refresh\" content=\"1;url=../homepage.html\">
+                </head>
+            </html>
+            
+            ");
+        }
+        else
+        {
+            if($_POST['mail'] == $mail && $_POST['pass'] == $pass)
+            {
+                $_SESSION['mail'] = $mail;
+
+            }
+            else
+            {
+                print("
+                
+                <script>
+                    alert(\"Email or Password incorrect\");
+                </script>
+
+                ");
+
+                print(" <script> location.href='../login.html'</script> ");
+            }
+        }
+    }
+    else
+    {
+        echo " <h4> No user present </h4>";
+    
+    }  
 
 ?>
